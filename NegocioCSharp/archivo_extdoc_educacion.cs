d42b10777e13862722cf4744b93c6b33a4a84e02
Stefano Trejo <stefanots@gmail.com>
2022-10-12 09:39:20 -0300

    Generacion de archivos CGE y Ganancias

--- NegocioCSharp/archivo_extdoc_educacion.cs
+++ NegocioCSharp/archivo_extdoc_educacion.cs
@@ -66,6 +66,10 @@ namespace LiquidacionSueldos.Negocio
                 Fila = ocdGestor.EjecutarReader("[ExtensionDocente.Archivo_Educacion]", new object[,] {
                 });
                 CrearArchivo(Fila);
+
+                Fila = ocdGestor.EjecutarReader("[ExtensionDocente.Archivo_Ganancias]", new object[,] {
+                }); 
+                Crear_Archivo_Ganancias(Fila);
                 /*
                 if (Fila.Rows.Count > 0)
                 {
@@ -321,8 +325,124 @@ namespace LiquidacionSueldos.Negocio
                                                 + row["C08"].ToString().Trim()
                                                 + row["I08"].ToString().Trim();
                         }
-                                                
-                        string linea = row["encabezado"].ToString().Trim() + cantidad_items + conceptos_importes;
+
+                        string cantidad_items_string = cantidad_items.ToString();
+                        if (cantidad_items < 10)
+                            cantidad_items_string = "0" + cantidad_items;
+
+                        string linea = row["encabezado"].ToString().Trim() + cantidad_items_string + conceptos_importes;
+                        file.WriteLine(linea);
+
+                    }
+                }                
+            }
+            catch (Exception oError)
+            {
+                string error = @"<div class=""alert alert-danger alert-dismissable"">
+<button aria-hidden=""true"" data-dismiss=""alert"" class=""close"" type=""button"">x</button>
+<a class=""alert-link"" href=""#"">Error de Sistema</a><br/>
+Se ha producido el siguiente error:<br/>
+MESSAGE:<br>" + oError.Message + "<br><br>EXCEPTION:<br>" + oError.InnerException + "<br><br>TRACE:<br>" + oError.StackTrace + "<br><br>TARGET:<br>" + oError.TargetSite +
+               "</div>";
+            }
+        }
+
+        protected void Crear_Archivo_Ganancias(DataTable dt)
+        {
+            try
+            {
+                String fecha = DateTime.Today.ToString("dd/MM/yyyy").Replace('/', '-');
+                string Ruta = "C:/Users/stefano/Desktop/ganancias_extdoc";
+                if (System.IO.File.Exists(Ruta + fecha + ".txt"))
+                {
+                    File.SetAttributes(Ruta + fecha + ".txt", FileAttributes.Normal);
+                    File.Delete(Ruta + fecha + ".txt");
+                }
+                using (StreamWriter file = new StreamWriter(Ruta += fecha + ".txt", true))
+                {
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        int cantidad_items = 0;
+                        string conceptos_importes = "";
+                        string prueba = row["C01"].ToString();
+
+                        if (row["C01"].ToString().Trim().Length > 0
+                            && row["I01"].ToString().Trim() != "000000000")
+                        {
+                            cantidad_items++;
+                            conceptos_importes = conceptos_importes
+                                                + row["C01"].ToString().Trim()
+                                                + row["I01"].ToString().Trim();
+                        }
+
+                        if (row["C02"].ToString().Trim().Length > 0
+                           && row["I02"].ToString().Trim() != "000000000")
+                        {
+                            cantidad_items++;
+                            conceptos_importes = conceptos_importes
+                                                + row["C02"].ToString().Trim()
+                                                + row["I02"].ToString().Trim();
+                        }
+
+                        if (row["C03"].ToString().Trim().Length > 0
+                             && row["I03"].ToString().Trim() != "000000000")
+                        {
+                            cantidad_items++;
+                            conceptos_importes = conceptos_importes
+                                                + row["C03"].ToString().Trim()
+                                                + row["I03"].ToString().Trim();
+                        }
+
+                        if (row["C04"].ToString().Trim().Length > 0
+                             && row["I04"].ToString().Trim() != "000000000")
+                        {
+                            cantidad_items++;
+                            conceptos_importes = conceptos_importes
+                                                + row["C04"].ToString().Trim()
+                                                + row["I04"].ToString().Trim();
+                        }
+
+                        if (row["C05"].ToString().Trim().Length > 0
+                              && row["I05"].ToString().Trim() != "000000000")
+                        {
+                            cantidad_items++;
+                            conceptos_importes = conceptos_importes
+                                                + row["C05"].ToString().Trim()
+                                                + row["I05"].ToString().Trim();
+                        }
+
+                        if (row["C06"].ToString().Trim().Length > 0
+                              && row["I06"].ToString().Trim() != "000000000")
+                        {
+                            cantidad_items++;
+                            conceptos_importes = conceptos_importes
+                                                + row["C06"].ToString().Trim()
+                                                + row["I06"].ToString().Trim();
+                        }
+
+                        if (row["C07"].ToString().Trim().Length > 0
+                              && row["I07"].ToString().Trim() != "000000000")
+                        {
+                            cantidad_items++;
+                            conceptos_importes = conceptos_importes
+                                                + row["C07"].ToString().Trim()
+                                                + row["I07"].ToString().Trim();
+                        }
+
+                        if (row["C08"].ToString().Trim().Length > 0
+                              && row["I08"].ToString().Trim() != "000000000")
+                        {
+                            cantidad_items++;
+                            conceptos_importes = conceptos_importes
+                                                + row["C08"].ToString().Trim()
+                                                + row["I08"].ToString().Trim();
+                        }
+
+                        string cantidad_items_string = cantidad_items.ToString();
+                        if (cantidad_items < 10)
+                            cantidad_items_string = "0" + cantidad_items;
+
+                        string linea = row["encabezado"].ToString().Trim() + cantidad_items_string + conceptos_importes;
                         file.WriteLine(linea);
 
                     }
@@ -338,5 +458,6 @@ MESSAGE:<br>" + oError.Message + "<br><br>EXCEPTION:<br>" + oError.InnerExceptio
                "</div>";
             }
         }
+
     }
 }
