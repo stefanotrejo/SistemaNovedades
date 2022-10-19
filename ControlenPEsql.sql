8f9ade6752ba4e545cd5a653d63815a7b4614fbc
Stefano Trejo <stefanots@gmail.com>
2022-10-19 19:58:52 -0300

    Avances

---
+++ ControlenPEsql.sql
@@ -0,0 +1,55 @@
+use LiquidacionSueldos
+
+select * from PagosEventuales 
+
+select * from DetallePagoEventual
+
+aguantame
+
+declare @dni decimal(11)
+set @dni='38365672'
+
+SELECT 
+	* 
+FROM 
+	PagosEventuales
+WHERE substring(ageCUIT, 3, 8) in (
+ @dni
+)
+
+
+SELECT 
+	* 
+FROM 
+	"PagosEventuales_13.7.22_servidor"
+WHERE substring(ageCUIT, 3, 8) in (
+@dni
+)
+
+
+SELECT 
+	* 
+FROM 
+	"PagosEventuales_14.2.22"
+WHERE substring(ageCUIT, 3, 8) in (
+@dni
+)
+
+
+SELECT 
+	* 
+FROM 
+	"PagosEventuales_30.6.22"
+WHERE substring(ageCUIT, 3, 8) in (
+@dni
+)
+
+SELECT 
+	* 
+FROM 
+	"PagosEventuales_Sucio"
+WHERE substring(ageCUIT, 3, 8) in (
+@dni
+)
+
+select * from DetallePagoEventual
